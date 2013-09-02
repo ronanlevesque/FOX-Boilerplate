84034ba0a7bbc3d25499d84077ad56870b93b158
eskiiss <ronan.levesque@gmail.com>
2013-09-02 17:04:35 +0200

    Updated tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
     compass: {
       dev: {
